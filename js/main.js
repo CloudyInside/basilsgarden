6276517b38c1103c929e974b72a7ca31925073dd
CloudyInside <claudiasmenacho@gmail.com>
2025-07-13 01:46:39 +0200

    rebuilding my site from scratch with Hugo for better and quicker maintenance - WIP

---
+++ js/main.js
@@ -0,0 +1,46 @@
+(() => {
+  // <stdin>
+  document.addEventListener("DOMContentLoaded", () => {
+    const imgs = document.querySelectorAll(".gallery img");
+    let currentIndex = 0;
+    const fullScreen = document.getElementById("fullscreen");
+    const leftArrow = document.createElement("div");
+    const rightArrow = document.createElement("div");
+    leftArrow.textContent = " ";
+    rightArrow.textContent = " ";
+    leftArrow.className = "arrow left-arrow";
+    rightArrow.className = "arrow right-arrow";
+    fullScreen.appendChild(leftArrow);
+    fullScreen.appendChild(rightArrow);
+    function updateFullScreenImage(index) {
+      if (index < 0) {
+        currentIndex = imgs.length - 1;
+      } else if (index >= imgs.length) {
+        currentIndex = 0;
+      } else {
+        currentIndex = index;
+      }
+      console.log(imgs);
+      fullScreen.style.backgroundImage = `url('${imgs[currentIndex].getAttribute("data-full")}')`;
+      fullScreen.style.display = "block";
+    }
+    imgs.forEach((img, index) => {
+      img.addEventListener("click", function(event) {
+        event.stopPropagation();
+        updateFullScreenImage(index);
+      });
+    });
+    leftArrow.addEventListener("click", function(event) {
+      event.stopPropagation();
+      updateFullScreenImage(currentIndex - 1);
+    });
+    rightArrow.addEventListener("click", function(event) {
+      event.stopPropagation();
+      updateFullScreenImage(currentIndex + 1);
+    });
+    fullScreen.addEventListener("click", function(event) {
+      event.stopPropagation();
+      this.style.display = "none";
+    });
+  });
+})();
